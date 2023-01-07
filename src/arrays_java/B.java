a7a622fd827e4a03c1b30ed91e66ab879f60f339
Shanthkumar-H <shannhiremath14@gmail.com>
2023-01-07 14:27:20 +0530

    array

---
+++ src/arrays_java/B.java
@@ -0,0 +1,10 @@
+package arrays_java;
+@FunctionalInterface
+ public interface B {
+	final int x=10;
+	
+	  default  void  B() {
+	}
+	  void c();
+	}
+
---
+++ src/arrays_java/C.java
@@ -0,0 +1,11 @@
+package arrays_java;
+public class C {
+	int x=10/0;
+public static void main(String[] args) {
+
+	String s="100";
+int x=Integer.parseInt(s);
+System.out.println(x);
+
+}
+}
